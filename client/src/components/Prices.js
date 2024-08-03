6d2f2318e5563e64e7b47703f045cf466fc4fe4d
sanjanamasam1210 <sanjanamasam1210@gmail.com>
2024-08-03 18:15:43 +0530

    added files

---
+++ client/src/components/Prices.js
@@ -0,0 +1,27 @@
+export const Prices = [
+  {
+    _id: 0,
+    name: "$0 to $4",
+    array: [0, 4],
+  },
+  {
+    _id: 1,
+    name: "$5 to $9",
+    array: [5, 9],
+  },
+  {
+    _id: 2,
+    name: "$10 to $14",
+    array: [10, 14],
+  },
+  {
+    _id: 3,
+    name: "$15 to $19",
+    array: [15, 19],
+  },
+  {
+    _id: 4,
+    name: "$20 or more",
+    array: [20, 9999],
+  },
+];
